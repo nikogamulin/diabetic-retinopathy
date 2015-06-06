805c37d4dd40cfd8f278e8c81f5edc2a6b1dee00
nikogamulin <niko.gamulin@gmail.com>
2015-06-06 11:10:27 +0200

    added script to generate subsample dataset

---
+++ image_list_modifier.py
@@ -0,0 +1,22 @@
+'''
+Created on 6. jun. 2015
+
+@author: GamulinN
+'''
+
+import common
+
+exampleLine = "run-normal_12_left.jpeg 12"
+imageListSource = "/media/niko/data/data/DiabeticRetinopathyDetection/labelsTest.txt"
+imageListDestination = "/media/niko/data/data/DiabeticRetinopathyDetection/labelsTest_run-normal.txt"
+imagesToInclude = ["run-normal"]
+itemsDestination = []
+imagesCount, itemsSource = common.getItemsFromFile(filename = imageListSource, excludeHeader = False)
+for item in itemsSource:
+    for configTag in imagesToInclude:
+        if configTag in item:
+            common.storeItem(item, imageListDestination)
+            
+print "Process completed, list stored to %s." % imageListDestination
+    
+
